61f5159ff7b3405765c9788f5ecf80719b025bcb
NataliaPienio <90185181+NataliaPienio@users.noreply.github.com>
2023-09-12 09:21:50 +0200

    Add files via upload

---
+++ delegaty/delegaty/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace delegaty
+{
+    public delegate void MySampleDelegate(int number);
+    public static class MyDelegate
+    {
+        public static MySampleDelegate mySampleDelegate;
+        public static void Main()
+        {
+            mySampleDelegate += SampleMethod;
+            mySampleDelegate?.Invoke(10);
+        }
+
+        public static void SampleMethod(int number) {
+        Console.WriteLine("Podana liczba: "+number);
+        }
+    }
+
+}
